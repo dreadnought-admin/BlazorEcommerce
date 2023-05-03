15d69c5247a01c17f8146e8c398cb1e8e5374f87
dreadnought-admin <dom.alexan@gmail.com>
2023-05-03 17:04:18 -0600

    Routes properly to details page

---
+++ BlazorEcommerce/Server/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorEcommerce.Server.Services.CategoryService
+{
+    public interface ICategoryService
+    {
+        Task<ServiceResponse<List<Category>>> GetCategories();
+
+
+    }
+}
